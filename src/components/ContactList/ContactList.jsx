8ba8c07f06b85c591e5a6319f336e2d14eda8be8
KalininVitalii <105376147+KalininVitalii@users.noreply.github.com>
2022-11-06 14:55:07 -0500

    done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectedContacts, selectedFilter } from 'redux/contact-selectors';
 import { deleteContact } from 'redux/contacts-slice';
 import { List, Button } from './ContactList.styled';
+import Avatar from 'react-avatar';
 
 export const ContactList = () => {
   const contacts = useSelector(selectedContacts);
@@ -16,6 +17,9 @@ export const ContactList = () => {
     <ul>
       {filterList.map(({ id, name, number }) => (
         <List key={id}>
+          <span>
+            <Avatar round={true} size={25} name={name} />
+          </span>
           <span>
             {name}: {number}
           </span>
